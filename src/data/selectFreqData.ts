12f0af1b5398553005fbd74a8305ba2dcd75a5ae
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-10-18 15:13:58 -0300

    new: opções de frequencia de dosagem

---
+++ src/data/selectFreqData.ts
@@ -0,0 +1,9 @@
+import { SelectFreqType } from "../types/SelectFreqType";
+
+export const selectItems: SelectFreqType[] = [
+  { id: 0, title: "1x ao dia" },
+  { id: 1, title: "2x ao dia" },
+  { id: 2, title: "3x ao dia" },
+  { id: 3, title: "A cada 8h" },
+  { id: 4, title: "A cada 12h" },
+];
